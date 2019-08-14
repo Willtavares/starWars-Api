2f6afdeb81f87137aa6daf37e39d93948ed9c309
@willtavares <willtavares@outlook.com>
2019-08-14 16:11:05 -0300

    Insert list Planets

--- src/controllers/planetController.js
+++ src/controllers/planetController.js
@@ -3,6 +3,19 @@ const Planets = require('../models/planets')
 
 const router = express.Router()
 
+router.get('/', async(req, res) => {
+    try {
+        const data = await Planets.find({})
+            .then(data => {
+                res.status(200).send(data)
+        })
+        return data
+    } catch (error) {
+        return res.status(400).send({error: 'Registration Failed'})
+    }
+
+})
+
 router.post('/register', async(req, res) => {
     const { name } = req.body
 
